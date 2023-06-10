63c55488d3f92992e29548c3fbf122d03b712d72
Mikhail Babaychev <109237708+MikiHousse@users.noreply.github.com>
2023-06-10 21:05:52 +0300

    add selectTodo and refactoring

--- src/components/TodoApp/TodoApp.tsx
+++ src/components/TodoApp/TodoApp.tsx
@@ -1,37 +1,86 @@
 'use client';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import cls from './TodoApp.module.scss';
 import { observer } from 'mobx-react-lite';
 import { ITodo } from '@/store/TodoStore';
 import todoStore from '@/store/TodoStore';
+import { AiOutlinePlus } from 'react-icons/ai';
 
 const TodoApp: FC = observer(() => {
+	const [addTodo, setAddTodo] = useState(false);
+	const [todoInfo, setTodoInfo] = useState(false);
+
+	const handleAddTodoClick = () => {
+		setAddTodo(true);
+		setTodoInfo(false);
+	};
+
 	return (
 		<div className={cls.TodoApp}>
+			{/* <div>
+				<form>
+					<input type="text" />
+					<button onClick={todoStore.addSubtodo(el.id , el.name)}></button>
+				</form>
+			</div> */}
 			<div className={cls.todoInfo}>
-				<ul>
-					{todoStore.todoList.map((el: ITodo) => (
-						<li key={el.id} className={cls.task} onClick={() => todoStore.showTodo(el.id)}>
-							<span>{el.name}</span>
-							{el.show ? <p>{el.description}</p> : ''}
-
-							<input type='checkbox' defaultChecked={el.done} />
-						</li>
-					))}
-				</ul>
 				<div>
-					{' '}
-					<form className={cls.input}>
-						<input
-							type='text'
-							value={todoStore.todo.name}
-							onChange={(e) => (todoStore.todo.name = e.target.value)}
-						/>
-						<input
-							type='textarea'
-							value={todoStore.todo.description}
-							onChange={(e) => (todoStore.todo.description = e.target.value)}
-						/>
+					{todoStore.todoList.map((el: ITodo) => (
+						<div
+							key={el.id}
+							className={cls.task}
+							onClick={() => {
+								todoStore.selectTodo(el.id);
+								setAddTodo(false);
+								setTodoInfo(true);
+							}}>
+							<div className={cls.list}>
+								<button onClick={() => todoStore.showTodo(el.id)}>^</button>
+								<span className={cls.name}>
+									{el.id}. {el.name}
+								</span>
+								<input className={cls.checkbox} type='checkbox' defaultChecked={el.done} />
+							</div>
+							{el.show ? (
+								<div>
+									{el.subtodo?.map((el) => {
+										return <span>{el.name}</span>;
+									})}
+								</div>
+							) : (
+								<form>
+									<input
+										type='text'
+										onChange={(e) => {
+											todoStore.todo.name = e.target.value;
+										}}
+									/>
+									<button
+										onClick={(e) => {
+											e.preventDefault();
+											todoStore.addSubtodo();
+										}}>
+										add
+									</button>
+								</form>
+							)}
+						</div>
+					))}
+				</div>
+				{!addTodo ? (
+					<div className={cls.forms}>
+						<form className={cls.input}>
+							<input
+								type='text'
+								value={todoStore.todo.name}
+								onChange={(e) => (todoStore.todo.name = e.target.value)}
+							/>
+							<input
+								type='textarea'
+								value={todoStore.todo.description}
+								onChange={(e) => (todoStore.todo.description = e.target.value)}
+							/>
+						</form>
 						<button
 							onClick={(e) => {
 								e.preventDefault();
@@ -39,9 +88,22 @@ const TodoApp: FC = observer(() => {
 							}}>
 							Add Task
 						</button>
-					</form>
-				</div>
+					</div>
+				) : (
+					''
+				)}
+				{todoInfo ? (
+					<div>
+						<h2>{todoStore.todo.name}</h2>
+						<p>{todoStore.todo.description}</p>
+					</div>
+				) : (
+					''
+				)}
 			</div>
+			<button className={cls.addTodo} onClick={handleAddTodoClick}>
+				<AiOutlinePlus color='white' size={20} />
+			</button>
 		</div>
 	);
 });
